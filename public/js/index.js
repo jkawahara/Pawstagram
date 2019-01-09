51acd2b61dcd5c967b9e2c4570b4b2aaa5569abe
John Kawahara <kawaharajohn@gmail.com>
2019-01-09 14:31:42 -0800

    Add initial api routing User, Pet

--- public/js/index.js
+++ public/js/index.js
@@ -1,25 +1,62 @@
 // Wait for DOM to be fully loaded before attaching handlers
 $(function() {
-  // $("#submit").on("click", function(event) {
-  //   var id = $(this).data("id");
-  //   var newDevour = $(this).data("newdevour");
+  $("#submit").on("click", function(event) {
+    var id = $(this).data("id");
+    var newDevour = $(this).data("newdevour");
 
-  //   var newDevourState = {
-  //     devoured: newDevour
-  //   };
-  // });
+    var newDevourState = {
+      devoured: newDevour
+    };
+  });
 
-  $(".new-user").on("#submit", function(event) {
-    console.log($(this).attr("id"));
+  $(".new-user").on("submit", function(event) {
+    
     // Make sure to preventDefault on a submit event.
     event.preventDefault();
-    
+
     var newUser = {
+      userPhotoUrl: $("#user-url").val().trim(),
       name: $("#user-name").val().trim(),
       email: $("#user-email").val().trim(),
       password: $("#user-password").val().trim(),
       communities: $("#user-communities").val().trim(),
     };
-    console.log(newUser);
-  });
+    console.log(newUser);  
+      // HTTP POST request to add new burger
+      $.ajax("/api/users", {
+        type: "POST",
+        data: newUser
+      }).then(
+        function() {
+          console.log("created new user");
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+
+  $(".new-pet").on("submit", function(event) {
+    // Make sure to preventDefault on a submit event.
+    event.preventDefault();
+    
+    var newPet = {
+      name: $("#pet-name").val().trim(),
+      species: $("#pet-species").val().trim(),
+      age: parseInt($("#pet-age").val().trim()),
+      profPic: $("#pet-profPic").val().trim(),
+      UserId: 1
+    };
+    console.log(newPet);  
+      // HTTP POST request to add new burger
+      $.ajax("/api/pets", {
+        type: "POST",
+        data: newPet
+      }).then(
+        function() {
+          console.log("created new pet");
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
 });
