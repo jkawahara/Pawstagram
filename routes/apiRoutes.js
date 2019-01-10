9aceb288301fbf828a7ac879907b3326e93377ce
John Kawahara <kawaharajohn@gmail.com>
2019-01-10 12:24:08 -0800

    Add comm page test

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -22,6 +22,29 @@ module.exports = function(app) {
     });
   });
 
+  // Get all comms
+  app.get("/api/comms", function(req, res) {
+    db.Community.findAll({}).then(function(dbComm) {
+      res.json(dbComm);
+    });
+  });
+
+  // Create a new comm
+  app.post("/api/comms", function(req, res) {
+    db.Community.create(req.body).then(function(dbComm) {
+      res.json(dbComm);
+    });
+  });
+
+  // Delete a comm by id
+  app.delete("/api/comms/:id", function(req, res) {
+    db.Community.destroy({ where: { id: req.params.id } }).then(function(
+      dbComm
+    ) {
+      res.json(dbComm);
+    });
+  });
+
   // Get all pets
   app.get("/api/pets", function(req, res) {
     db.Pet.findAll({}).then(function(dbPets) {
